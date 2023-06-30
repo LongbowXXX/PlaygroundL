b51a55f1250a52b5a816af3799d64eb4c2661297
LongbowXXX <50514445+LongbowXXX@users.noreply.github.com>
2023-06-30 21:13:50 +0900

    Implement Google Custom Search.

---
+++ search/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm")
+    `java-library`
+}
+
+group = "net.longbowxxx.search"
+version = "${rootProject.property("package.version")}"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.google.api-client:google-api-client:${property("google.api.client")}")
+    implementation("com.google.apis:google-api-services-customsearch:${property("google.services.customsearch")}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${property("kotlinx.coroutine.core.version")}")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:${property("kotlinx.coroutine.core.version")}")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
